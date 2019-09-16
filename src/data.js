f97c765b4f70c9e37e099d0912f1fa0c20c7a88f
juhsaolno <juanita.hsolano@gmail.com>
2019-09-16 13:25:24 -0300

    atualizacao HTML, data.js filtrando indicadores por pais

--- src/data.js
+++ src/data.js
@@ -3,75 +3,25 @@ window.data = {
 }
 
 function countries(chosenCountry) {
-    let selectedCountry = "";
+    let selectedCountry = ""; 
+    console.log(chosenCountry);
     for (let i in chosenCountry) {
         if (chosenCountry[i].checked) {
-            if (chosenCountry[i].value == "chile") {
-                console.log("Chile!");
-                // return WORLDBANK.CHL;
-            } else if (chosenCountry[i].value == "brasil") {
-                console.log("Brasil!");
-                // return WORLDBANK.BRA;   
-            } else if (chosenCountry[i].value == "mexico") {
-                console.log ("México!");
-                // return WORLDBANK.MEX;
-            } else if (chosenCountry[i].value == "peru") {
-                console.log ("Peru!");
-                // return WORLDBANK.PER;
-            } else {
-                console.log("nada escolhido");
-            }
+            const pais = chosenCountry[i].value;
+            selectedCountry += myIndicators(WORLDBANK[pais].indicators);
+            console.log(myIndicators(WORLDBANK[pais].indicators));
         }
     }
     return selectedCountry;  
 };
 
+//Função para passar por todos os elementos de countri e retornar o indicatorName
+function myIndicators(countries){
+    return countries.map((elemento)=>elemento.indicatorName);
+}
 
-//Colocando país dentro de uma variável - até indicadores
-const chile = WORLDBANK.CHL.indicators;
-
-//Para acessar os CÓDIGOS dos indicadores
-
-//Sem formato arrow function
-// const indicadores = chile.map(function(indCode){
-//     return indCode.indicatorCode;
-// })
-
-const indCode = chile.map((code) => code.indicatorCode);
-
-//Para verificar os codigos - Usar apenas um como exemplo
-function verifyCode (indCode) {
-    for (let i of indCode) {
-        if (indCode == "SL.TLF.PART.FE.ZS"){
-            return true;
-        } else {
-            return false;
-        }
-    }
-};
-//Colocar os codes desejados em um array!
-
-//Para filtrar o código 
-const filteringCode = indCode.filter(verifyCode);
-
-//Devolvendo dados para o código selecionado
-
-// const returningData 
-
-//Pegando dados
-const srcData = chile.map((myData)=> myData.data);
-
-
-// filtrar e rodar map?
-
-//Filtrar anos
-function selectedYears (years){
-    return years >= 2012;
-};
-
-const teste = srcData.filter(selectedYears);
-
-
-srcData.forEach(year => {
-    
-});
+//Criando arrays com os códigos
+const myCodes = ["SL.TLF.CACT.FM.NE.ZS", "SL.TLF.BASC.ZS", "SL.TLF.BASC.MA.ZS", "SL.TLF.BASC.FE.ZS", 
+"SL.TLF.INTM.ZS", "SL.TLF.INTM.MA.ZS", "SL.TLF.INTM.FE.ZS", "SL.TLF.ADVN.ZS", "SL.TLF.ADVN.MA.ZS", 
+"SL.TLF.ADVN.FE.ZS", "SL.UEM.TOTL.NE.ZS", "SL.UEM.TOTL.MA.NE.ZS", "SL.UEM.TOTL.FE.NE.ZS", "SL.UEM.INTM.FE.ZS", 
+"SL.UEM.BASC.FE.ZS", "SL.UEM.ADVN.FE.ZS"];
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 function processingInput () {
     event.preventDefault();
-    // const chosenChile = document.getElementById("chosen-chile");
-    // const chosenBrasil = document.getElementById("chosen-brasil");
-    // const chosenMexico = document.getElementById("chosen-mexico");
-    // const chosenPeru = document.getElementById("chosen-peru");
     const chosenCountry = document.getElementsByName("country");
     const chosenCountries = window.data.countries(chosenCountry); //, chosenChile, chosenBrasil, chosenMexico, chosenPeru);
-    document.getElementById("chosenCountry").innerHTML = `<p> Escolheu: ${chosenCountry}</p>`;
+    document.getElementById("myChosenCountry").innerHTML = `<p> Escolheu: ${chosenCountries}</p>`;
 } 
 document.getElementById("data-processing").addEventListener("click", processingInput);
