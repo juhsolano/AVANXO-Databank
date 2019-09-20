6a3deb4792f366a736b9350ea3800f23c76e7ea6
juhsaolno <juanita.hsolano@gmail.com>
2019-09-20 19:29:57 -0300

    atualizacao dos filtros(pais e indicador) e alteracoes nos testes

--- src/data.js
+++ src/data.js
@@ -1,70 +1,16 @@
 window.data = {
   countries: countries
+};
+
+function countries(chosenCountry, chosenIndicator, chosenYears) {
+  //Transformando 
+  const checkboxOptions = Array.from(chosenCountry);
+  //Retorna em forma de array
+  //Listando os checados
+  const checkedCountry = checkboxOptions.filter(i => i.checked === true).map(i => i.value);
+  const indicatorForCountry = checkedCountry.map(pais => {
+    return WORLDBANK[pais].indicators.filter(i => i.indicatorCode.includes(chosenIndicator.value));
+  });
+  console.log(indicatorForCountry);
+  const selectedYear = indicatorForCountry.
 }
-
-//Função para passar por todos os elementos de country e retornar o indicatorCode
-function myIndicators(countries) {
-  return countries.map((element) => element.indicatorCode);
-};
-
-// Função para acessar os dados([data])
-function myData(countries) {
-  return countries.map((element) => element.data);
-};
-
-//Colocando em uma variável meus códigos desejados
-const myIndicatorsCode = ["SL.TLF.CACT.FM.NE.ZS", "SL.TLF.BASC.ZS", "SL.TLF.BASC.MA.ZS", "SL.TLF.BASC.FE.ZS",
-  "SL.TLF.INTM.ZS", "SL.TLF.INTM.MA.ZS", "SL.TLF.INTM.FE.ZS", "SL.TLF.ADVN.ZS",
-  "SL.TLF.ADVN.MA.ZS", "SL.TLF.ADVN.FE.ZS", "SL.UEM.TOTL.NE.ZS", "SL.UEM.TOTL.MA.NE.ZS",
-  "SL.UEM.TOTL.FE.NE.ZS", "SL.UEM.BASC.FE.ZS", "SL.UEM.INTM.FE.ZS", "SL.UEM.ADVN.FE.ZS"];
-
-  const myYears = [2012, 2013, 2014, 2015, 2016, 2017];
-
-//Filtrar os códigos do meu banco de acordo com meus códigos desejados 
-function filteredCode(indCode) {
-  const selectedIndicators = indCode.filter((code) => { return myIndicatorsCode.includes(code) });
-  return selectedIndicators;
-};
-
-//Filtrar anos desejados 
-function filteredYears(years) {
-  const selectedYears = years.filter((year) => { return myYears.includes(year) });
-  return selectedYears;
-};
-
-// function filterData(dataResult) {
-//   const dataForIndicators = dataResult.filter((data) => {return codeOptions.includes(data)});
-//   return dataForIndicators;
-// };
-
-function countries(chosenCountry, chosenIndicator) {
-    const xuxu = Array.from(chosenCountry)
-    console.log(xuxu.map(i => i))
-    console.log(xuxu.map(i => i.checked))
-
-    se (xuxu[i].checked === true)
-    const pais = xuxu.value 
-      arrayIndices[pais].indicators.filter(filtrar por chosenIndicator)
-
-  let selectedCountry = "";
-  for (let i in chosenCountry) {
-    if (chosenCountry[i].checked) {
-      const country = chosenCountry[i].value;
-      console.log(country);
-      const allCountries = WORLDBANK[country].indicators;
-      const indicatorsByCountry = myIndicators(allCountries);
-      const data = myData(allCountries);
-      console.log(data);
-      const codeOptions = filteredCode(indicatorsByCountry);
-      selectedCountry += codeOptions;
-    }
-  }
-  return selectedCountry;
-};
-
-// for (let i in chosenIndicator){
-//   if (chosenIndicator[i].select)
-// }
-// const dataByCountry = chosenIndicator.value;
-// console.log(dataByCountry);
-// chosenIndicator.value 
--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ function processingInput() {
   event.preventDefault();
   const chosenCountry = document.getElementsByName("country");
   const chosenIndicator = document.getElementById("select-indicators");
-  const chosenCountries = window.data.countries(chosenCountry, chosenIndicator);//countries(chosenCountry);
+  const chosenYears = document.getElementsByName("year");
+  const chosenCountries = window.data.countries(chosenCountry, chosenIndicator, chosenYears);
   document.getElementById("myChosenCountry").innerHTML = `<p> Escolheu: ${chosenCountries}</p>`;
-  console.log(chosenIndicator.value);
+  // console.log(chosenIndicator.value);
 }
 document.getElementById("data-processing").addEventListener("click", processingInput);
 
