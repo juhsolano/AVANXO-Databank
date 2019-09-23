4d9ba8c98bc9ccdd9c7ae38065c7db015fbf16db
juhsaolno <juanita.hsolano@gmail.com>
2019-09-23 13:54:39 -0300

    adicao de filtro de data e adequacao do HTML

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 function processingInput() {
   event.preventDefault();
-  const chosenCountry = document.getElementsByName("country");
+  const chosenCountry = document.getElementsByName("select-country");
   const chosenIndicator = document.getElementById("select-indicators");
-  const chosenYears = document.getElementsByName("year");
-  const chosenCountries = window.data.countries(chosenCountry, chosenIndicator, chosenYears);
-  document.getElementById("myChosenCountry").innerHTML = `<p> Escolheu: ${chosenCountries}</p>`;
-  // console.log(chosenIndicator.value);
+  const chosenYears = document.getElementsByName("select-year");
+  const processedData = window.data.countries(chosenCountry, chosenIndicator, chosenYears);
+  document.getElementById("myChosenCountry").innerHTML = `<p> Escolheu: ${processedData}</p>`;
 }
 document.getElementById("data-processing").addEventListener("click", processingInput);
 
