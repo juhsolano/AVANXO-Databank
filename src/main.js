7ef3f37f7397c3c236b3c8ca13817d02129ad50b
juhsaolno <juanita.hsolano@gmail.com>
2019-09-25 14:06:08 -0300

    filtro de anos funcionando e CSS ajustado

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ function processingInput() {
   const chosenCountry = document.getElementsByName("select-country");
   const chosenIndicator = document.getElementById("select-indicators");
   const chosenYears = document.getElementsByName("select-year");
-  const printing = document.getElementById("data-processed");
-  const processedData = window.data.filterInfo(chosenCountry, chosenIndicator, chosenYears, printing);
+  const processedData = window.data.filterInfo(chosenCountry, chosenIndicator, chosenYears);
 }
 document.getElementById("data-processing").addEventListener("click", processingInput);
 
@@ -13,63 +12,32 @@ window.data = {
 };
 
 function filterInfo(chosenCountry, chosenIndicator, chosenYears, printing) {
-  // const myResults = [];
   const checkboxOptions = Array.from(chosenCountry);
-  // const checkboxYear = Array.from(chosenYears);
+  const checkboxYear = Array.from(chosenYears);
   const checkedCountry = checkboxOptions.filter(i => i.checked === true).map(i => i.value);
-  // const checkedYear = checkboxYear.filter(i => i.checked === true).map(i => i.value);
+  const checkedYear = checkboxYear.filter(i => i.checked === true).map(i => i.value);
   const indicatorForCountry = checkedCountry.map(country => {
     return WORLDBANK[country].indicators.filter(i => i.indicatorCode.includes(chosenIndicator.value));
   });
-  const data = indicatorForCountry.map(elementSelected => {
-    return elementSelected.map(item => Object.entries(item.data).filter(year => year >= "2012"));
-  });
-  const dataResults = [];
-  indicatorForCountry.map(elementSelected => {
-    elementSelected.map(item => {
-      dataResults.push(
-        {
-          country: item.countryName,
-          indicator: item.indicatorName,
-          information: data
-        });
-    });
-  });
-  console.log(dataResults);
-  // return dataResults;
-
-  const print = printing;
-  print.innerHTML += `<div>
-     <p>${dataResults[0].country}</p> </div >`
-
-  //----------------------------------------------------------------------------
-  //----------------------------------------------------------------------------
-  //#########TODA A SÉRIE DE ANOS E VALORES PARA TODOS OS PAÍSES#########
-  // const data = indicatorForCountry.map(elementSelected => {
-  //   return elementSelected.map(item => Object.keys(item.data).map(dt => {
-  //     return console.log(dt, item.data[dt]);
-  //   }));
-  // });
-  // const myFilteredData = data.filter(year => year >= "2012");
-  // console.log(myFilteredData);
-  //######################################
-
-  //#########PEGA O ANO DESEJADO DE QUALQUER PÁIS, MAS JOGA NUM ÚNICO ARRAY#########
-  // const data = indicatorForCountry.map(elementSelected => {
-  //   return elementSelected.map(item => Object.entries(item.data));
-  // });
-  // const takingValue = data.map(item => item[0].map(dataList => (dataList)).filter(key => {
-  //   if (checkedYear.includes(key[0])) {
-  //     return myResults.push(key[1]);
-  //   }
-  // }));
-  // return console.log(myResults);
-  //######################################
-
-  //#########PEGA UM ANO POR VEZ E JOGA NUM ÚNICO ARRAY#########
-  // const data = indicatorForCountry.map(elementSelected => {
-  //   return elementSelected.map(item => item.data)[0][checkedYear];
-  // });
-  // console.log(data);
-  //######################################
+  const dataProcessed = document.getElementById("data-processed");
+  dataProcessed.innerHTML += `
+   ${indicatorForCountry.map(pais => {
+    return `<div>
+             <p>${pais[0].countryName}</p>
+             <p>${pais[0].indicatorName}</p>
+             <p>${checkedYear.map(ano => `${ano} : ${pais[0].data[ano]}`)}</p>
+           </div > `
+  }).join("")}
+ `
+  //Processo lógico usando array
+  //   const dataProcessed = document.getElementById("data-processed");
+  //   dataProcessed.innerHTML += `
+  //    ${indicatorForCountry.map(pais => pais.map(i => {
+  //     return `<div>
+  //              <p>${i.countryName}</p>
+  //              <p>${i.indicatorName}</p>
+  //              <p>${checkedYear.map(ano => Object.entries(i.data).filter(i => i[0] === ano)).map(i => i.map(j => j[0] + ": " + j[1]))}</p>
+  //            </div > `
+  //   })).join("")}
+  //  `
 };
